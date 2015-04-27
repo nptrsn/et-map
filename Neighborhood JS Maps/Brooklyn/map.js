77fe1f3a4a74c378e5a4896213d2aa7860993d56
nptrsn <nick@hindiemedia.com>
2015-04-27 12:06:47 -0700

    Brooklyn: cleanup:

--- Neighborhood JS Maps/Brooklyn/map.js
+++ Neighborhood JS Maps/Brooklyn/map.js
@@ -4,8 +4,8 @@ var map;
 
 // Colour order: #FF0000, #0000FF, #FF6600, #FFFF00, #551A8B
 
-// Highlight Castro/Upper Market area
-function hoverCastro(){
+// Highlight Park Slope area
+function hoverParkSlope(){
 
   // Define the LatLng coordinates for the polygon's path
   var shapeCoords = [
@@ -34,7 +34,37 @@ function hoverCastro(){
 
   areaHighlight.setMap(map);
 }
+// Highlight Castro/Upper Market area
+function hoverCastro(){
 
+  // Define the LatLng coordinates for the polygon's path
+  var shapeCoords = [
+    new google.maps.LatLng(40.657500, -73.983050),
+  new google.maps.LatLng(40.660861, -73.980035),
+  new google.maps.LatLng(40.661097, -73.980078),
+  new google.maps.LatLng(40.661235, -73.979692),
+  new google.maps.LatLng(40.672179, -73.970722),
+  new google.maps.LatLng(40.672968, -73.970411),
+  new google.maps.LatLng(40.674156, -73.970700),
+  new google.maps.LatLng(40.674637, -73.970239),
+  new google.maps.LatLng(40.674995, -73.970851),
+  new google.maps.LatLng(40.684592, -73.977791),
+  new google.maps.LatLng(40.665204, -73.992404)
+  ];
+
+  // Construct the polygon
+  areaHighlight = new google.maps.Polygon({
+    paths: shapeCoords,
+    strokeColor: '#a0151f',
+    strokeOpacity: 0.3,
+    strokeWeight: 1,
+    fillColor: '#a0151f',
+    fillOpacity: 0.35
+  });
+
+  areaHighlight.setMap(map);
+}
+40.7035051,-73.989543
 ///////////////////
 // Clear the map
 function clearMap(){
