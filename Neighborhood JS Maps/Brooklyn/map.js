8d8dd38be4b44969cb89b1f4725625d207f2d0b5
nptrsn <nick@hindiemedia.com>
2015-04-27 12:07:34 -0700

    Oakland: Rockridge

--- Neighborhood JS Maps/Brooklyn/map.js
+++ Neighborhood JS Maps/Brooklyn/map.js
@@ -34,22 +34,28 @@ function hoverParkSlope(){
 
   areaHighlight.setMap(map);
 }
-// Highlight Castro/Upper Market area
-function hoverCastro(){
+// Highlight Dumbo area
+function hoverDumbo(){
 
   // Define the LatLng coordinates for the polygon's path
   var shapeCoords = [
-    new google.maps.LatLng(40.657500, -73.983050),
-  new google.maps.LatLng(40.660861, -73.980035),
-  new google.maps.LatLng(40.661097, -73.980078),
-  new google.maps.LatLng(40.661235, -73.979692),
-  new google.maps.LatLng(40.672179, -73.970722),
-  new google.maps.LatLng(40.672968, -73.970411),
-  new google.maps.LatLng(40.674156, -73.970700),
-  new google.maps.LatLng(40.674637, -73.970239),
-  new google.maps.LatLng(40.674995, -73.970851),
-  new google.maps.LatLng(40.684592, -73.977791),
-  new google.maps.LatLng(40.665204, -73.992404)
+  new google.maps.LatLng(40.705551, -73.984517),
+  new google.maps.LatLng(40.705396, -73.986394),
+  new google.maps.LatLng(40.704656, -73.986469),
+  new google.maps.LatLng(40.704672, -73.986662),
+  new google.maps.LatLng(40.705079, -73.986705),
+  new google.maps.LatLng(40.705071, -73.988186),
+  new google.maps.LatLng(40.704575, -73.988261),
+  new google.maps.LatLng(40.704656, -73.989570),
+  new google.maps.LatLng(40.704835, -73.989688),
+  new google.maps.LatLng(40.704908, -73.990214),
+  new google.maps.LatLng(40.704623, -73.992155),
+  new google.maps.LatLng(40.704575, -73.993625),
+  new google.maps.LatLng(40.704217, -73.994580),
+  new google.maps.LatLng(40.702615, -73.992563),
+  new google.maps.LatLng(40.702566, -73.991694),
+  new google.maps.LatLng(40.701695, -73.990568),
+  new google.maps.LatLng(40.701443, -73.984785)
   ];
 
   // Construct the polygon
@@ -64,7 +70,7 @@ function hoverCastro(){
 
   areaHighlight.setMap(map);
 }
-40.7035051,-73.989543
+
 ///////////////////
 // Clear the map
 function clearMap(){
