5357f1bb049117a92c7441ee5ac16d3e98bec1d3
nptrsn <nick@hindiemedia.com>
2015-04-28 12:11:00 -0700

    Brooklyn: williamsburg

--- Neighborhood JS Maps/Brooklyn/map.js
+++ Neighborhood JS Maps/Brooklyn/map.js
@@ -34,6 +34,37 @@ function hoverParkSlope(){
 
   areaHighlight.setMap(map);
 }
+
+// Highlight Williamsburg area
+function hoverWilliamsburg(){
+
+  // Define the LatLng coordinates for the polygon's path
+  var shapeCoords = [
+    new google.maps.LatLng(40.657500, -73.983050),
+  new google.maps.LatLng(40.660861, -73.980035),
+  new google.maps.LatLng(40.661097, -73.980078),
+  new google.maps.LatLng(40.661235, -73.979692),
+  new google.maps.LatLng(40.672179, -73.970722),
+  new google.maps.LatLng(40.672968, -73.970411),
+  new google.maps.LatLng(40.674156, -73.970700),
+  new google.maps.LatLng(40.674637, -73.970239),
+  new google.maps.LatLng(40.674995, -73.970851),
+  new google.maps.LatLng(40.684592, -73.977791),
+  new google.maps.LatLng(40.665204, -73.992404)
+  ];
+
+  // Construct the polygon
+  areaHighlight = new google.maps.Polygon({
+    paths: shapeCoords,
+    strokeColor: '#a0151f',
+    strokeOpacity: 0.3,
+    strokeWeight: 1,
+    fillColor: '#a0151f',
+    fillOpacity: 0.35
+  });
+
+  areaHighlight.setMap(map);
+}
 // Highlight Dumbo area
 function hoverDumbo(){
 
