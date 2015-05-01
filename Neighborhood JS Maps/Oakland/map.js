fc0077ecb98de8ed1c46acfb5c26955f27bc6a7a
nptrsn <nick@hindiemedia.com>
2015-05-01 13:04:55 -0700

    Oakland: Grand Lake

--- Neighborhood JS Maps/Oakland/map.js
+++ Neighborhood JS Maps/Oakland/map.js
@@ -159,6 +159,38 @@ function hoverPiedmont(){
 
   areaHighlight.setMap(map);
 }
+// Highlight Grand Lake area
+function hoverGrandLake(){
+
+  // Define the LatLng coordinates for the polygon's path
+  var shapeCoords = [
+    new google.maps.LatLng(37.834908, -122.252894),
+  new google.maps.LatLng(37.835908, -122.255083),
+  new google.maps.LatLng(37.836535, -122.257078),
+  new google.maps.LatLng(37.836925, -122.260190),
+  new google.maps.LatLng(37.837450, -122.262893),
+  new google.maps.LatLng(37.838111, -122.264803),
+  new google.maps.LatLng(37.837840, -122.265919),
+  new google.maps.LatLng(37.837840, -122.265919),
+  new google.maps.LatLng(37.837264, -122.267078),
+  new google.maps.LatLng(37.829977, -122.269030),
+  new google.maps.LatLng(37.828773, -122.260555),
+  new google.maps.LatLng(37.828265, -122.258430),
+  new google.maps.LatLng(37.827892, -122.257336),
+  ];
+
+  // Construct the polygon
+  areaHighlight = new google.maps.Polygon({
+    paths: shapeCoords,
+    strokeColor: '#a0151f',
+    strokeOpacity: 0.3,
+    strokeWeight: 1,
+    fillColor: '#a0151f',
+    fillOpacity: 0.35
+  });
+
+  areaHighlight.setMap(map);
+}
 
 ///////////////////
 // Clear the map
