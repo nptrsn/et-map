08fa58380bcd7634cb54f50970465cae752643de
nptrsn <nick@hindiemedia.com>
2015-07-09 19:23:19 -0700

    header and uri fixes

--- WB/w-b-theme/header.php
+++ WB/w-b-theme/header.php
@@ -1,5 +1,18 @@
 <?php
 /**
+ * The Header for our theme
+ * See @ framework/hooks/actions.php for all actions attached to your header hooks.
+ *
+ * IMPORTANT :	There isn't any need to modify this template file, most edits can't be done via hooks
+ * 				and filters	or the partial template parts at partials/header/.
+ *
+ * @package		Total
+ * @subpackage	Templates
+ * @author		Alexander Clarke
+ * @copyright	Copyright (c) 2015, WPExplorer.com
+ * @link		http://www.wpexplorer.com
+ * @since		Total 1.0.0
+ * @version		2.0.0
  */ ?>
  
 <!DOCTYPE html>
